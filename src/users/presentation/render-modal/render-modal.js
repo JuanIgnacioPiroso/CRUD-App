7630ea4a38171ca7dee5ed3065ea6f0656b4c05f
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-18 16:16:37 -0300

    form values saved

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,7 +1,7 @@
 import modalHtml from "./render-modal.html?raw";
 import "./render-modal.css";
 
-let modal,form;
+let modal, form;
 
 export const showModal = () => {
   modal?.classList.remove("hide-modal");
@@ -9,6 +9,7 @@ export const showModal = () => {
 
 export const hideModal = () => {
   modal?.classList.add("hide-modal");
+  form?.reset();
 };
 
 /**
@@ -30,9 +31,25 @@ export const renderModal = (element) => {
     hideModal();
   });
 
-  form.addEventListener("submit",(event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
-  })
+
+    const formData = new FormData(form);
+    const userLike = {};
+
+    for (const [key, value] of formData) {
+      if (key === "balance") {
+        userLike[key] = +value;
+        continue;
+      }
+      if (key === "isActive") {
+        userLike[key] = value === "on" ? true : false;
+        continue;
+      }
+      userLike[key] = value;
+    }
+    hideModal();
+  });
 
   element.append(modal);
 };
