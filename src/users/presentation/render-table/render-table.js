086933fc0fca158c285feac07d2b0af0b0e9d0f3
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-18 17:19:41 -0300

    select user implemented

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,5 +1,6 @@
 import "./render-table.css";
 import usersStore from "../../store/users-store";
+import { showModal } from "../render-modal/render-modal";
 
 let table;
 
@@ -24,6 +25,18 @@ const createTable = () => {
 
 /**
  *
+ * @param {MouseEvent} event
+ */
+const tableSelectListener = (event) => {
+  const element = event.target.closest(".select-user");
+  if (!element) return;
+
+  const id = element.getAttribute("data-id");
+  showModal(id);
+};
+
+/**
+ *
  * @param {HTMLDivElement} element
  */
 export const renderTable = (element) => {
@@ -32,6 +45,8 @@ export const renderTable = (element) => {
   if (!table) {
     table = createTable();
     element.append(table);
+
+    table.addEventListener("click", tableSelectListener);
   }
 
   let tableHTML = "";
@@ -44,9 +59,9 @@ export const renderTable = (element) => {
     <td>${user.lastName}</td>
     <td>${user.isActive}</td>
     <td>
-    <a href = "#/ data-id="${user.id}" =>Select</a>
+    <a href = "#/" class="select-user" data-id="${user.id}" =>Select</a>
 
-    <a href = "#/ data-id="${user.id}" >Delete</a>
+    <a href = "#/" class="delete-user" data-id="${user.id}" >Delete</a>
     </td>
     </tr>
     `;
